08138681ea64220bcbff1ebdd51c1c3358761fad
Wiktor-Siemiatkowski <wiktor.siemiatkowski20@gmail.com>
2022-09-08 23:50:42 +0200

    Add change button

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 const container = document.querySelector('.container');
+const dimBtn = document.querySelector('.dimbtn');
+let n = 16;
 
-for (let i = 0; i < 256; i++) {
-    const cell = document.createElement('div');
-    cell.classList.add('cell');
-    container.append(cell);
-    cell.addEventListener('mouseover', () => {
-        cell.style.backgroundColor = 'black';
-    })
+function etchASketch() {
+
+dimBtn.addEventListener('click', () => {
+    n = prompt('Define grid side: ');
+    container.style.gridTemplateColumns = `repeat(${n}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${n}, 1fr)`;
+    container.innerHTML = '';
+    createGrid();
+})
+
+createGrid();
+}
+
+
+function createGrid() {
+    for (let i = 0; i < n ** 2; i++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        container.append(cell);
+    
+        cell.addEventListener('mouseover', () => {
+            cell.style.backgroundColor = 'black';
+        });
+    };
 };
+
+etchASketch();
